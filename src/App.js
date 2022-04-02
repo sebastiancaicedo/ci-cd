0d4d3b31a94dd2f2c50deb7ff86c450e1c1ef115
Luis Caicedo <sebastiancaicedo@uninorte.edu.co>
2022-04-02 10:49:56 -0500

    learn react text modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Web with Nodejs
         </a>
       </header>
     </div>
