6a48b2acce1a718c98b0c81cc79ca866c8e2f0f9
Luis Caicedo <sebastiancaicedo@uninorte.edu.co>
2022-04-02 10:53:32 -0500

    Fix test reset Learn React text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Web with Nodejs
+          Learn React
         </a>
       </header>
     </div>
